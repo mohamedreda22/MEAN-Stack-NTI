18e545ffb4acbd6539aa761eadeec47f42e1136e
Mohamed Reda <73208810+mohamedreda22@users.noreply.github.com>
2024-12-19 14:37:10 +0200

    Add .npmignore files and update package.json to include multer dependency

--- express/productRouter.js
+++ express/productRouter.js
@@ -1,18 +1,58 @@
-const express =require("express")
-
+const express =require("express");
+const multer = require("multer");
 const router = express.Router();
 const ProductsArray =[    
-    {id:1,name:"Phone",description:"description of the Phone"},
-    {id:2,name:"card",description:"description of the card"},
-    {id:3,name:"ssss",description:"description of the ssss"},
+    {id:1,name:"Phone",description:"description of the Phone",imgURL:"img1.png"},
+    {id:2,name:"card",description:"description of the card",imgURL:"img2.png"},
+    {id:3,name:"ssss",description:"description of the ssss",imgURL:"img3.jpg"},
 ]
+
 router.get('/',(req,res)=>{
-    res.json(ProductsArray)
+    if(req.query.id ){
+        const product = ProductsArray.find(p=>p.id == req.query.id)
+        if(!product){
+            return res.status(404).json({error:"Product not found"})
+        }else {
+            return res.json(product);
+        }
+    }else if(req.query.name){
+        const product = ProductsArray.find(p=>p.name == req.query.name)
+        return res.json(product);
+    }else{
+        res.status(200).json(ProductsArray);
+    }
 })
-router.post('/',(req,res)=>{
-    // const {id,name,description }= req.body
-    ProductsArray.push(req.body)
-    res.json(req.body)
+router.get('/:id',(req,res)=>{
+    const product = ProductsArray.find(p=>p.id == req.params.id)
+    res.json(product);
+})
+
+
+router.delete('/:id',(req,res)=>{
+    const index = ProductsArray.findIndex(p=>p.id == req.params.id)
+    if(index == -1){
+        return res.status(404).json({error:"Product not found"})
+    }
+    ProductsArray.splice(index,1)
+    res.status(204).json()
+})
+
+const storage = multer.diskStorage({destination: (req,file,cb)=>{
+    cb(null,'images');
+},filename: (req,file,cb)=>{
+    cb(null,Date.now() + "_" + file.originalname)
+}});
+const upload =multer({storage});
+
+router.post('/',upload.single('productImage'),(req,res)=>{
+    try{
+        const {id, name, description}=req.body;
+        const imageName = req.file.filename;
+    ProductsArray.push({id, name, description ,imgURL:imageName})
+    res.status(201).json(req.body)
+    }catch(error){
+        res.status(500).json('error: ',{error: error.message})
+    }
 })
 
 module.exports = router;
