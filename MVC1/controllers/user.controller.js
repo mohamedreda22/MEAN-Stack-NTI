da67fbcd94d099ec958621bdce04915ec63d422b
Mohamed Reda <73208810+mohamedreda22@users.noreply.github.com>
2024-12-24 14:15:41 +0200

    Refactor loginUser controller to destructure request body and improve readability

--- MVC1/controllers/user.controller.js
+++ MVC1/controllers/user.controller.js
@@ -70,13 +70,30 @@ exports.deleteAllUsers = async (req, res) => {
     }
 }
 
+// exports.loginUser = async (req, res) => {
+//     try {
+//         const user = await userModel.findOne({ email: req.body.email });
+//         if (!user) {
+//             return res.status(404).json({ message: 'User not found' });
+//         }
+//         const passwordMatch = await isMatch(req.body.password, user.password);
+//         if (!passwordMatch) {
+//             return res.status(400).json({ message: 'Invalid credentials' });
+//         }
+//         res.status(200).json({ message: 'Login successful' });
+//     }
+//     catch (error) {
+//         res.status(500).json({ message: error.message });
+//     }
+// }
 exports.loginUser = async (req, res) => {
     try {
-        const user = await userModel.findOne({ email: req.body.email });
+        const {email,password} = req.body;
+        const user = await userModel.findOne({ email: email });
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-        const passwordMatch = await isMatch(req.body.password, user.password);
+        const passwordMatch = await isMatch(password, user.password);
         if (!passwordMatch) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
