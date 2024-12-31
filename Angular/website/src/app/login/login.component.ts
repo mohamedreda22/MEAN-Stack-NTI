2346f91671dee055b001ae95d3b8fe796c89b731
Mohamed Reda <73208810+mohamedreda22@users.noreply.github.com>
2024-12-31 14:59:24 +0200

    Implement authentication service and login functionality: add AuthService, integrate login method, and create login component

--- Angular/website/src/app/login/login.component.ts
+++ Angular/website/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -9,8 +10,17 @@ import { NgForm } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private _authS:AuthService) { }
 
   login(loginForm:NgForm) {
+    this._authS.login(loginForm.value).subscribe({
+      next: () => {
+        console.log('success');
+      },
+      error: (err: any) => {
+        console.log('error :>> ', err.message);
+      }
+    });
     console.log(loginForm.value);
   }
 }
